6cb62e8c3cdff56f00fb496ce5c8987d19e98623
Shiyue Wang <shiyue.wang@me.com>
2015-03-06 15:22:29 +0100

    add confirmation dialog

--- www/js/services.js
+++ www/js/services.js
@@ -28,4 +28,19 @@ angular.module('starter.services', [])
       return $http.get('//'+cameraAddress+':'+zahoPort+'/api/status');
     }
   };
-});
+})
+
+.directive('ngConfirmClick', [
+  function(){
+    return {
+      link: function (scope, element, attr) {
+        var msg = attr.ngConfirmClick || "Are you sure?";
+        var clickAction = attr.confirmedClick;
+        element.bind('click',function (event) {
+          if ( window.confirm(msg) ) {
+            scope.$eval(clickAction)
+          }
+        });
+      }
+    };
+  }]);
