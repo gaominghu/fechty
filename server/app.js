77115a806657043fafb31638d2d3ee9dd11b7428
gabrielstuff <gabriel@soixantecircuits.fr>
2015-03-10 10:37:13 +0100

    some changes

--- server/app.js
+++ server/app.js
@@ -299,7 +299,7 @@ app.get('/ping/all', function(req, res) {
 
   function addData(data) {
     response.push(data);
-    if (response.length >= 59) {
+    if (response.length >= hostNumber) {
       res
         .status(200)
         .json({
